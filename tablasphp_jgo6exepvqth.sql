a1036ee1be977c78c52d0c9feb59afac2af960e7
Patricia-Apaza <119316887+Patricia-Apaza@users.noreply.github.com>
2024-04-06 11:09:59 -0500

    Add files via upload

---
+++ tablasphp_jgo6exepvqth.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2024 a las 17:59:04
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_ofertas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulantes`
+--
+
+CREATE TABLE `postulantes` (
+  `id` int(11) NOT NULL,
+  `dni` varchar(15) NOT NULL DEFAULT '',
+  `nombre` varchar(30) NOT NULL DEFAULT '',
+  `apellido` varchar(30) NOT NULL DEFAULT '',
+  `edad` int(2) DEFAULT NULL,
+  `correo` varchar(100) NOT NULL DEFAULT '',
+  `telefono` varchar(10) DEFAULT '',
+  `direccion` varchar(50) NOT NULL DEFAULT '',
+  `estado_civil` varchar(20) NOT NULL DEFAULT '',
+  `especialidad` varchar(130) NOT NULL DEFAULT '',
+  `archivo_cv` varchar(30) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `postulantes`
+--
+ALTER TABLE `postulantes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `postulantes`
+--
+ALTER TABLE `postulantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
